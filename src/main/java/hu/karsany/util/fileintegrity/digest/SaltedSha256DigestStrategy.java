6c716e02cd7baaa7d777a3daf200bcd2804d3303
quwisky <nagy.bence1337@gmail.com>
2020-06-11 19:23:31 +0200

    refact(SaltedSha256DigestStrategy): promote MessageDigest to class level field

--- src/main/java/hu/karsany/util/fileintegrity/digest/SaltedSha256DigestStrategy.java
+++ src/main/java/hu/karsany/util/fileintegrity/digest/SaltedSha256DigestStrategy.java
@@ -1,19 +1,26 @@
 package hu.karsany.util.fileintegrity.digest;
 
-import hu.karsany.util.fileintegrity.digest.DigestStrategy;
+import hu.karsany.util.fileintegrity.digest.exception.DigestException;
 
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 public class SaltedSha256DigestStrategy implements DigestStrategy {
 
     private final String salt;
+    private final MessageDigest digest;
 
     public SaltedSha256DigestStrategy(String salt) {
         this.salt = salt;
+        try {
+            this.digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new DigestException(e);
+        }
     }
 
 
@@ -22,17 +29,17 @@ public class SaltedSha256DigestStrategy implements DigestStrategy {
         try {
             byte[] buffer = new byte[8192];
             int count;
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
             BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
             while ((count = bis.read(buffer)) > 0) {
-                digest.update(buffer, 0, count);
+                this.digest.update(buffer, 0, count);
             }
             bis.close();
 
-            digest.update(salt.getBytes());
+            this.digest.update(this.salt.getBytes());
 
-            byte[] hash = digest.digest();
-            return Base64.getEncoder().encodeToString(hash);
+            byte[] hash = this.digest.digest();
+            return Base64.getEncoder()
+                         .encodeToString(hash);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
