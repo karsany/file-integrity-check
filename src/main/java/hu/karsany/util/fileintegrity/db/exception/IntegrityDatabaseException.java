6490b255bcf4dd2843ce1bbaf576cb04d31bf924
quwisky <nagy.bence1337@gmail.com>
2020-06-11 19:26:11 +0200

    feat: add IntegrityDatabaseException

---
+++ src/main/java/hu/karsany/util/fileintegrity/db/exception/IntegrityDatabaseException.java
@@ -0,0 +1,8 @@
+package hu.karsany.util.fileintegrity.db.exception;
+
+public class IntegrityDatabaseException extends RuntimeException {
+
+    public IntegrityDatabaseException(Throwable cause) {
+        super(cause);
+    }
+}
